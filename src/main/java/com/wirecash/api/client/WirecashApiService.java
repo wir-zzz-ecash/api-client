f1882bf4975a35eb8d1db5228f8d53f841dd2838
lagrz <roarbort@gmail.com>
2017-08-21 14:52:53 -0700

    adds authorization header to quote request

--- src/main/java/com/wirecash/api/client/WirecashApiService.java
+++ src/main/java/com/wirecash/api/client/WirecashApiService.java
@@ -1,6 +1,7 @@
 package com.wirecash.api.client;
 
 import com.wirecash.api.models.Request.AuthenticateRequest;
+import com.wirecash.api.models.Request.QuoteRequest;
 import com.wirecash.api.models.Request.RefreshTokenRequest;
 import com.wirecash.api.models.Responses.AuthenticateResponse;
 import com.wirecash.api.models.Responses.CompaniesResponse;
@@ -19,8 +20,14 @@ public interface WirecashApiService {
     @GET("company/{id}")
     Call<CompaniesResponse> getCompany(@Path("id") Long id, @Header("Authorization") String access_token);
 
+    @GET("company/{id}")
+    Call<CompaniesResponse> getCompany(@Path("id") Long id, @Query("amount") Double amount, @Header("Authorization") String access_token);
+
     @GET("country/{country_code}")
     Call<CompaniesResponse> getServicesByCountry(@Path("country_code") String country_code, @Header("Authorization") String access_token);
+
+    @GET("country/{country_code}")
+    Call<CompaniesResponse> getServicesByCountry(@Path("country_code") String country_code, @Query("amount") Double amount, @Header("Authorization") String access_token);
 
     @GET("country/list")
     Call<CountryListResponse> getCountryList(@Header("Authorization") String access_token);
@@ -28,9 +35,18 @@ public interface WirecashApiService {
     @GET("state/{state_code}")
     Call<CompaniesResponse> getServicesByState(@Path("state_code") String state_code, @Header("Authorization") String access_token);
 
+    @GET("state/{state_code}")
+    Call<CompaniesResponse> getServicesByState(@Path("state_code") String state_code, @Query("amount") Double amount, @Header("Authorization") String access_token);
+
     @GET("state/{state_code}/country/{country_code}")
     Call<CompaniesResponse> getCompany(@Path("state_code") String state_code, @Path("country_code") String country_code, @Header("Authorization") String access_token);
 
+    @GET("state/{state_code}/country/{country_code}")
+    Call<CompaniesResponse> getCompany(@Path("state_code") String state_code, @Path("country_code") String country_code, @Query("amount") Double amount, @Header("Authorization") String access_token);
+
     @GET("state/list")
     Call<StateListResponse> getStateList(@Header("Authorization") String access_token);
+
+    @POST("quote")
+    Call<QuoteRequest> getQuote(@Body QuoteRequest quoteRequest, @Header("Authorization") String access_token);
 }
